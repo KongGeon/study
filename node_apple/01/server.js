f10357ce19caaebc96bd1890d11ad79cb5628314
SnagGeon <yangsk0526@gmail.com>
2023-05-03 18:03:05 +0900

    0503 노드 공부 02

--- node_apple/01/server.js
+++ node_apple/01/server.js
@@ -7,9 +7,13 @@ app.listen(8081, function () {
     console.log('8081')
 });
 
+//get 요청
 app.get('/pet', function (요청, 응답) {
     응답.send('펫용품 쇼핑')
 })
 app.get('/beauty', function (요청, 응답) {
     응답.send('뷰티용품 사세요!')
+})
+app.get('/', function (요청, 응답) { 
+    응답.sendFile(__dirname + '/index.html')
 })
