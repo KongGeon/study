942768624680e8f2af0cefb686022c62ce2ccd93
KongGeon <66816095+KongGeon@users.noreply.github.com>
2023-05-04 00:08:02 +0900

    0504 노드

--- node_apple/01/server.js
+++ node_apple/01/server.js
@@ -1,19 +1,28 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+app.use(express.urlencoded({extended: true})); //body-parser 불러오기
 
 app.listen(8081, function () {
-    //8080 서버 열렸을때 함수실행
+  //8080 서버 열렸을때 함수실행
 
-    console.log('8081')
+  console.log("8081");
 });
 
 //get 요청
-app.get('/pet', function (요청, 응답) {
-    응답.send('펫용품 쇼핑')
-})
-app.get('/beauty', function (요청, 응답) {
-    응답.send('뷰티용품 사세요!')
-})
-app.get('/', function (요청, 응답) { 
-    응답.sendFile(__dirname + '/index.html')
-})
+app.get("/pet", function (요청, 응답) {
+  응답.send("펫용품 쇼핑");
+});
+app.get("/beauty", function (요청, 응답) {
+  응답.send("뷰티용품 사세요!");
+});
+app.get("/", function (요청, 응답) {
+  응답.sendFile(__dirname + "/index.html");
+});
+app.get("/write", function (요청, 응답) {
+  응답.sendFile(__dirname + "/write.html");
+});
+
+//post 요쳥
+app.post("/add", function (요청, 응답) {
+  응답.send(요청.body);
+});
